fce5e3e0531a96259e2238d50b62ddbe0e319ea6
ManuFTC1 <122982411+ManuFTC1@users.noreply.github.com>
2023-01-25 20:48:10 +0200

    Adaugat init
    Acum Gogu se incadreaza in dimensiunile regulamentare in timpul init.

--- RobotTeleopPOV_Linear.java
+++ RobotTeleopPOV_Linear.java
@@ -1,35 +1,5 @@
-/* Copyright (c) 2017 FIRST. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted (subject to the limitations in the disclaimer below) provided that
- * the following conditions are met:
- *
- * Redistributions of source code must retain the above copyright notice, this list
- * of conditions and the following disclaimer.
- *
- * Redistributions in binary form must reproduce the above copyright notice, this
- * list of conditions and the following disclaimer in the documentation and/or
- * other materials provided with the distribution.
- *
- * Neither the name of FIRST nor the names of its contributors may be used to endorse or
- * promote products derived from this software without specific prior written permission.
- *
- * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
- * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.util.ElapsedTime;
@@ -38,16 +8,29 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 public class RobotTeleopPOV_Linear extends LinearOpMode {
 
     RobotHardware robot = new RobotHardware();
-    private ElapsedTime runtime = new ElapsedTime();
+    ElapsedTime runtime = new ElapsedTime();
 
+    double acceleratie;
     double pozitieMaini = 0.74;
+
+    public void pornire() {
+
+        telemetry.addLine("S-a trezit Gogu");
+
+        while (runtime.seconds() < 0.5) {//dupa init bratu sus, se desfac mainile, bratu jos
+            robot.bratRobot.setPower(0.5);
+        }
+        robot.setServoPosition(pozitieMaini);
+
+        while (runtime.seconds() < 0.7) {
+            robot.bratRobot.setPower(0.5);
+        }
+    }
 
     @Override
     public void runOpMode() {
 
         String modCondus = "normal";
-        String modBrate = "normal";
-
         robot.init(hardwareMap);
 
         telemetry.addLine("Robot PREGATIT");
@@ -58,41 +41,35 @@ public class RobotTeleopPOV_Linear extends LinearOpMode {
         while (opModeIsActive()) {
 
             //rotile lui Sergiu
-            if(modCondus.equals("normal"))
+            acceleratie = 0;
+
+            if (modCondus.equals("normal"))
                 modNormal();
             else
                 modManual();
 
             if (gamepad1.left_bumper)
                 modCondus = "manual";
-            if(gamepad1.right_bumper)
+            if (gamepad1.right_bumper)
                 modCondus = "normal";
 
             //bratul lui Manu
             robot.bratRobot.setPower(gamepad2.right_stick_y);
 
-            if(modBrate == "normal") {
-                pozitieMaini += gamepad2.right_trigger * 0.01;
-                pozitieMaini -= gamepad2.left_trigger * 0.01;
+            pozitieMaini += gamepad2.right_trigger * 0.01;
+            pozitieMaini -= gamepad2.left_trigger * 0.01;
 
-                pozitieMaini = Math.max(pozitieMaini, 0.75);
+            pozitieMaini = Math.max(pozitieMaini, 0.25);//sa nu depaseaca valori de 1 sau -1
+            pozitieMaini = Math.min(pozitieMaini, 1);
 
-                if (gamepad2.right_bumper)
-                    pozitieMaini = 1.0;
-                if (gamepad2.left_bumper)
-                    pozitieMaini = 0.75;
+            if (gamepad2.right_bumper)
+                pozitieMaini = 1.0;
+            if (gamepad2.left_bumper)
+                pozitieMaini = 0.75;
+            if (gamepad2.a)
+                pozitieMaini = 0.25;
 
-                robot.setServoPosition(pozitieMaini);
-            }
-            else {
-                robot.setServoPosition(0.25);
-            }
-
-            if(gamepad2.dpad_up)
-                modBrate = "normal";
-
-            if(gamepad2.dpad_down)
-                modBrate = "stop";
+            robot.setServoPosition(pozitieMaini);
 
             //telemetry
             chat();
@@ -102,13 +79,10 @@ public class RobotTeleopPOV_Linear extends LinearOpMode {
 
             sleep(20);//incetineste sa nu fie prea rapid
         }
-
         stop();
     }
 
     public void modManual(){//joysticku stang - roata stanga, joysticku drept - roata dreapta
-        float rotire1;
-        float rotire2;
         float rotireaAiurea;
 
         rotireaAiurea = gamepad1.left_stick_x;
@@ -118,26 +92,23 @@ public class RobotTeleopPOV_Linear extends LinearOpMode {
             return;
         }
 
-        float vitezaStanga;
-        float vitezaDrepta;
-
-        rotire1 = gamepad1.left_stick_x;
-        rotire2 = gamepad1.right_stick_x;
+        double vitezaStanga;
+        double vitezaDrepta;
 
         vitezaStanga = -gamepad1.left_stick_y;
         vitezaDrepta = -gamepad1.right_stick_y;
 
         if(gamepad1.right_trigger == 0 ) {
-            robot.motorStanga.setPower(Math.min(vitezaStanga, 0.75));
-            robot.motorDreapta.setPower(Math.min(vitezaDrepta, 0.75));
+            robot.motorStanga.setPower(Math.min(vitezaStanga * acceleratie, 0.75));
+            robot.motorDreapta.setPower(Math.min(vitezaDrepta * acceleratie, 0.75));
         }
     }
 
     public void modNormal() {//rt viteza, lt marsalier/frana, joystick stanga directie
 
-        float viteza;
-        float viraj;
-        float rotireAiurea;
+        double viteza;
+        double viraj;
+        double rotireAiurea;
 
         rotireAiurea = gamepad1.left_stick_x;
 
@@ -149,42 +120,58 @@ public class RobotTeleopPOV_Linear extends LinearOpMode {
         viraj = gamepad1.left_stick_x;
 
         if(viteza == 0)
-            robot.setWheelsPower(0);
+            putereAmbeleMotoare(0);
 
         if(viraj == 0)
-            robot.setWheelsPower(viteza);
+            putereAmbeleMotoare(viteza);
         else {
             if (viraj < 0) {
                 franareDreapta(viteza, viraj);
-                robot.motorDreapta.setPower(viteza);
+                putereMotorDreapta(viteza);
             }
             if (viraj > 0) {
                 franareStanga(viteza, viraj);
-                robot.motorStanga.setPower(viteza);
+                putereMotorStanga(viteza);
             }
         }
         telemetry.addData("viteza: ", viteza);
 
     }
 
-    public void franareStanga(float viteza, float viraj) {
-        double putereFrana;
+    public void putereMotorStanga(double viteza) {
+        acceleratie += viteza /23;//numar mai pica => acceleratie mai rapida
 
-        if(viteza > 0) {
-            putereFrana = viteza - viraj;
-            if(putereFrana < 0.1) {
-                putereFrana = -0.1;
-            }
-        }
-        else {
-            putereFrana = viteza + viraj;
-            if(putereFrana >-0.1)
-                putereFrana = 0.1;
-        }
-        robot.motorStanga.setPower(putereFrana);
+        viteza = viteza + acceleratie;
+
+        robot.motorStanga.setPower(viteza);
     }
 
-    public void franareDreapta(float viteza, float viraj) {
+    public void putereMotorDreapta(double viteza) {
+        acceleratie += viteza /23;//numar mai pica => acceleratie mai rapida
+
+        viteza = viteza + acceleratie;
+
+        robot.motorDreapta.setPower(viteza + acceleratie);
+    }
+
+    public void franareStanga(double viteza, double viraj) {
+        double putereFrana;
+
+        if(viteza > 0) {
+            putereFrana = viteza - viraj;
+            if(putereFrana < 0.1) {
+                putereFrana = -0.1;
+            }
+        }
+        else {
+            putereFrana = viteza + viraj;
+            if(putereFrana >-0.1)
+                putereFrana = 0.1;
+        }
+        robot.motorStanga.setPower(putereFrana * acceleratie);
+    }
+
+    public void franareDreapta(double viteza, double viraj) {
         double putereFrana;
 
         if(viteza > 0) {
@@ -198,7 +185,12 @@ public class RobotTeleopPOV_Linear extends LinearOpMode {
             if(putereFrana >-0.1)
                 putereFrana = 0.1;
         }
-        robot.motorDreapta.setPower(putereFrana);
+        robot.motorDreapta.setPower(putereFrana * acceleratie);
+    }
+
+    public void rotirePeLoc(double viteza) {//se roteste pe loc de pe joysticku din dreapta
+        robot.motorStanga.setPower(-viteza);
+        robot.motorDreapta.setPower(viteza);
     }
 
     public void chat() {
@@ -212,8 +204,8 @@ public class RobotTeleopPOV_Linear extends LinearOpMode {
             telemetry.addLine("Wow!");
     }
 
-    public void rotirePeLoc(float viteza) {//se roteste pe loc de pe joysticku din dreapta
-        robot.motorStanga.setPower(-viteza);
-        robot.motorDreapta.setPower(viteza);
+    public void putereAmbeleMotoare(double viteza) {
+        putereMotorDreapta(viteza);
+        putereMotorStanga(viteza);
     }
 }
