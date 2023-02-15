7561c66106f4f1b77a42aa7f3295ff6a466d28cf
ManuFTC1 <122982411+ManuFTC1@users.noreply.github.com>
2023-02-15 17:11:35 +0200

    Parcare frumoasa
    facilitati: -merge in fata
                  -parcheaza

---
+++ Vorba_aia.java
@@ -0,0 +1,232 @@
+package org.firstinspires.ftc.LTASrob.Autonomie;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.openftc.apriltag.AprilTagDetection;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+
+import org.firstinspires.ftc.LTASrob.RobotHardware;
+import org.firstinspires.ftc.LTASrob.OpModes.ReadRecord;
+
+import java.util.ArrayList;
+
+@Autonomous(name="Ursu latra, cainii merge", group="Robot")
+public class Vorba_aia extends LinearOpMode
+{
+    RobotHardware robot = new RobotHardware();
+    ElapsedTime runtime=new ElapsedTime();
+
+    OpenCvCamera camera;
+    AprilTagDetectionPipeline aprilTagDetectionPipeline;
+
+    static final double FEET_PER_METER = 3.28084;
+
+    // Lens intrinsics
+    // UNITS ARE PIXELS
+    // NOTE: this calibration is for the C920 webcam at 800x448.
+    // You will need to do your own calibration for other configurations!
+    double fx = 578.272;
+    double fy = 578.272;
+    double cx = 402.145;
+    double cy = 221.506;
+
+    // UNITS ARE METERS
+    double tagsize = 0.166;
+
+    //Tag ID-uri conuri
+    int Left = 15;
+    int Middle = 16;
+    int Right = 17;
+
+    AprilTagDetection tagOfInterest = null;
+
+    @Override
+    public void runOpMode()
+    {
+        robot.init(hardwareMap);
+
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        camera = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"), cameraMonitorViewId);
+        aprilTagDetectionPipeline = new AprilTagDetectionPipeline(tagsize, fx, fy, cx, cy);
+
+        camera.setPipeline(aprilTagDetectionPipeline);
+        camera.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener()
+        {
+            @Override
+            public void onOpened()
+            {
+                camera.startStreaming(800,448, OpenCvCameraRotation.UPRIGHT);
+            }
+
+            @Override
+            public void onError(int errorCode)
+            {
+
+            }
+        });
+
+        telemetry.setMsTransmissionInterval(50);
+
+        /*
+         * The INIT-loop:
+         * This REPLACES waitForStart!
+         */
+        while (!isStarted() && !isStopRequested())
+        {
+            ArrayList<AprilTagDetection> currentDetections = aprilTagDetectionPipeline.getLatestDetections();
+
+            if(currentDetections.size() != 0)
+            {
+                boolean tagFound = false;
+
+                for(AprilTagDetection tag : currentDetections)
+                {
+                    if(tag.id == Left || tag.id == Right || tag.id == Middle)
+                    {
+                        tagOfInterest = tag;
+                        tagFound = true;
+                        break;
+                    }
+                }
+
+                if(tagFound)
+                {
+                    telemetry.addLine("Tag of interest is in sight!\n\nLocation data:");
+                    tagToTelemetry(tagOfInterest);
+                }
+                else
+                {
+                    telemetry.addLine("Unde-i tagu??? :(");
+
+                    if(tagOfInterest == null)
+                    {
+                        telemetry.addLine("(The tag has never been seen)");
+                    }
+                    else
+                    {
+                        telemetry.addLine("\nBut we HAVE seen the tag before; last seen at:");
+                        tagToTelemetry(tagOfInterest);
+                    }
+                }
+
+            }
+            else
+            {
+                telemetry.addLine("Unde-i tagu??? :(");
+
+                if(tagOfInterest == null)
+                {
+                    telemetry.addLine("(The tag has never been seen)");
+                }
+                else
+                {
+                    telemetry.addLine("\nBut we HAVE seen the tag before; last seen at:");
+                    tagToTelemetry(tagOfInterest);
+                }
+
+            }
+
+            telemetry.update();
+            sleep(20);
+        }
+
+        /*
+         * The START command just came in: now work off the latest snapshot acquired
+         * during the init loop.
+         */
+
+        /* Update the telemetry */
+        if(tagOfInterest != null)
+        {
+            telemetry.addLine("Tag snapshot:\n");
+            tagToTelemetry(tagOfInterest);
+            telemetry.update();
+        }
+        else
+        {
+            telemetry.addLine("No tag snapshot available, it was never sighted during the init loop :(");
+            telemetry.update();
+        }
+
+        /* Actually do something useful */
+
+
+        telemetry.addData("secunde", runtime.seconds());
+        telemetry.update();
+
+        if (tagOfInterest == null || tagOfInterest.id == Middle) {
+                runtime.reset();
+                robot.brakeWheels(true);
+
+                while (runtime.seconds() < 1.3) {
+                    robot.setWheelsPower(0.5);
+                }
+
+                robot.brakeWheels(true);
+        }
+        else if(tagOfInterest.id == Left) {
+            runtime.reset();
+            robot.brakeWheels(true);
+
+            while(runtime.seconds() < 0.6) {
+                robot.setWheelsPower(1);
+            }
+            robot.setWheelsPower(0);
+            sleep(100);
+
+            while(runtime.seconds() < 1.15) {
+                robot.motorStanga.setPower(-0.9);
+                robot.motorDreapta.setPower(0.9);
+            }
+            while(runtime.seconds() < 1.25) {
+                robot.brakeWheels(true);
+            }
+            while(runtime.seconds() < 2.4) {
+                robot.setWheelsPower(0.5);
+            }
+            robot.brakeWheels(true);
+        }
+
+        else if(tagOfInterest.id == Right) {
+            runtime.reset();
+            robot.brakeWheels(true);
+
+            while(runtime.seconds() < 0.6) {
+                robot.setWheelsPower(1);
+            }
+            robot.setWheelsPower(0);
+            sleep(100);
+
+            while(runtime.seconds() < 1.15) {
+                robot.motorStanga.setPower(0.9);
+                robot.motorDreapta.setPower(-0.9);
+            }
+            while(runtime.seconds() < 1.25) {
+                robot.brakeWheels(true);
+            }
+            while(runtime.seconds() < 2.3) {
+                robot.setWheelsPower(0.5);
+            }
+            robot.brakeWheels(true);
+        }
+
+        /* You wouldn't have this in your autonomous, this is just to prevent the sample from ending */
+       // while (opModeIsActive()) {sleep(20);}
+    }
+
+    void tagToTelemetry(AprilTagDetection detection)
+    {
+        telemetry.addLine(String.format("\nDetected tag ID=%d", detection.id));
+        telemetry.addLine(String.format("Translation X: %.2f feet", detection.pose.x*FEET_PER_METER));
+        telemetry.addLine(String.format("Translation Y: %.2f feet", detection.pose.y*FEET_PER_METER));
+        telemetry.addLine(String.format("Translation Z: %.2f feet", detection.pose.z*FEET_PER_METER));
+        telemetry.addLine(String.format("Rotation Yaw: %.2f degrees", Math.toDegrees(detection.pose.yaw)));
+        telemetry.addLine(String.format("Rotation Pitch: %.2f degrees", Math.toDegrees(detection.pose.pitch)));
+        telemetry.addLine(String.format("Rotation Roll: %.2f degrees", Math.toDegrees(detection.pose.roll)));
+    }
+}
